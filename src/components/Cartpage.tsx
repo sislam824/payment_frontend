18a73fcdd5cdfd2405ad3cc8b1fcc553401636f4
Abu Osama <s.islam20000@gmail.com>
2024-11-01 05:43:10 +0530

    iniital commit

--- src/components/Cartpage.tsx
+++ src/components/Cartpage.tsx
@@ -50,7 +50,7 @@ const Cartpage: React.FC = () => {
         {
           items: cartItems.map((item) => ({
             title: item.title,
-            price: 0.01,
+            price: "0.01",
             quantity: item.quantity,
           })),
         }
